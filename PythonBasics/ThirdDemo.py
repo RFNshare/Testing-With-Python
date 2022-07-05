3c7f8e3f77bc7645e0637598c881d0400d37ef70
RFNshare <rfnshare@gmail.com>
2022-07-05 17:09:26 +0600

    For Loop

--- PythonBasics/ThirdDemo.py
+++ PythonBasics/ThirdDemo.py
@@ -6,3 +6,17 @@ else:
     print("Condition Do not match")
 
 print("If Else is completed")
+
+obj = [2, 3, 4, 5, 6, 7, 8, 9]
+for i in obj:
+    print(i)
+print("***************")
+a = 0
+for i in range(6):
+    a += i
+print(a)
+print("***************")
+for j in range(1,100,4):
+    print(j)
+
+print("***************")
