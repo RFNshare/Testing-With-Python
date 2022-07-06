0e8ba4d7a1ea90e5a8f1ccaea217ee3c474a9eea
RFNshare <rfnshare@gmail.com>
2022-07-06 21:40:44 +0600

    File read & readline

---
+++ PythonBasics/ReadWriteDemo.py
@@ -0,0 +1,10 @@
+file = open('../resources/sample.txt')
+
+# Read Number of characters by passing parameter
+print(file.read(25))
+
+# Read 1 line
+print(file.readline())
+file.close()
+
+# Print line by line
