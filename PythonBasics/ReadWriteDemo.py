5e212d89ca5ec3390f36d012757798a2b26cc21a
RFNshare <rfnshare@gmail.com>
2022-07-06 22:30:23 +0600

    File read with loop...

--- PythonBasics/ReadWriteDemo.py
+++ PythonBasics/ReadWriteDemo.py
@@ -1,10 +1,19 @@
 file = open('../resources/sample.txt')
 
 # Read Number of characters by passing parameter
-print(file.read(25))
-
-# Read 1 line
+# print(file.read(25))
+#
+# # Read 1 line
 print(file.readline())
-file.close()
+
 
 # Print line by line
+
+line = file.readline()
+print("***", line)
+for i in file.readline():
+    print(i)
+# while line != "":
+#     print(line)
+#     line = file.readline()
+file.close()
