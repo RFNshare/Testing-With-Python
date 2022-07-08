ebff4b3f10cebf85a8c1bebb11735c1c91d2bbd1
RFNshare <rfnshare@gmail.com>
2022-07-08 16:01:49 +0600

    Update Edge, Firefox Browser Testing Support dynamically

--- pythonSelenium/demoBrowser.py
+++ pythonSelenium/demoBrowser.py
@@ -7,34 +7,64 @@ from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
 from webdriver_manager.microsoft import EdgeChromiumDriverManager
 
-# service_obj = Service("../resources/chromedriver.exe")
 
-# Add some extra option for Chrome
-options = Options()
-options.add_argument("start-maximized")
+def run(browser, auto):
+    service_obj = ""
+    driver = ""
+    if browser in ("Chrome", "chrome", "c"):
+        # Add some extra option for Chrome
+        options = Options()
+        options.add_argument("start-maximized")
+        if auto in ("Yes", "yes", "ok"):
+            # Auto Download Driver & Saved into Cache
+            service_obj = ChromeService(ChromeDriverManager().install())
+        else:
+            # Manually Select driver from repository
+            service_obj = ChromeService("../resources/chromedriver.exe")
+        # Initialize Driver & Ready to go
+        driver = webdriver.Chrome(service=service_obj, options=options)
+    elif browser in ("Firefox", "firefox", "f"):
+        # Add some extra option for Firefox
+        # options = Options()
+        # options.add_argument("start-maximized")
+        if auto in ("Yes", "yes", "ok"):
+            # Auto Download Driver & Saved into Cache
+            service_obj = FirefoxService(GeckoDriverManager().install())
+        else:
+            # Manually Select driver from repository
+            service_obj = FirefoxService("../resources/geckodriver.exe")
+        # Initialize Driver & Ready to go
+        driver = webdriver.Firefox(service=service_obj)
+    elif browser in ("edge", "Edge", "e"):
+        # Add some extra option for Firefox
+        # options = Options()
+        # options.add_argument("start-maximized")
+        if auto in ("Yes", "yes", "ok"):
+            # Auto Download Driver & Saved into Cache
+            service_obj = EdgeService(EdgeChromiumDriverManager().install())
+        else:
+            # Manually Select driver from repository
+            service_obj = ChromeService("../resources/msedgedriver.exe")
+        # Initialize Driver & Ready to go
+        driver = webdriver.Edge(service=service_obj)
+    # Open browser & go to URL
+    URL = "https://rahulshettyacademy.com/"
+    driver.get(URL)
+    # assert webpage title
+    assert driver.title in "Rahul Shetty Academy"
+    # Checking Current URL
+    assert driver.current_url in URL
+    driver.get("https://www.rahulshettyacademy.com/AutomationPractice/")
+    # Minimize the window
+    driver.minimize_window()
+    # Back, Refresh & Forward the browser
+    driver.back()
+    driver.refresh()
+    driver.maximize_window()
+    driver.forward()
+    # Close the driver
+    driver.close()
 
-# Auto Download Driver & Saved into Cache
-chrome_service_obj = ChromeService(ChromeDriverManager().install())
-firefox_service_obj = FirefoxService(GeckoDriverManager().install())
-edge_service_obj = EdgeService(EdgeChromiumDriverManager().install())
 
-# Initialize Driver & Ready to go
-driver = webdriver.Edge(service=edge_service_obj)
-
-# Open browser & go to URL
-URL = "https://rahulshettyacademy.com/"
-driver.get(URL)
-# assert webpage title
-assert driver.title in "Rahul Shetty Academy"
-# Checking Current URL
-assert driver.current_url in URL
-driver.get("https://www.rahulshettyacademy.com/AutomationPractice/")
-# Minimize the window
-driver.minimize_window()
-# Back, Refresh & Forward the browser
-driver.back()
-driver.refresh()
-driver.maximize_window()
-driver.forward()
-# Close the driver
-driver.close()
+if __name__ == '__main__':
+    run('e', 'yes')
