17243aca0e5faf3bf6548bf7b60cf0f92377b11e
RFNshare <rfnshare@gmail.com>
2022-07-08 14:49:31 +0600

    Update driver saving method with webdriver module

--- pythonSelenium/demoBrowser.py
+++ pythonSelenium/demoBrowser.py
@@ -1,6 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
-service_obj = Service("../resources/chromedriver.exe")
+# service_obj = Service("../resources/chromedriver.exe")
+
+# Auto Download Driver & Saved into Cache
+service_obj = Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service_obj)
 driver.get("https://github.com/SeleniumHQ/selenium/tree/trunk/py")
