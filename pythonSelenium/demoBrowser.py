d2fb2919b36e9e36434372edecf52aefdeacbf7b
RFNshare <rfnshare@gmail.com>
2022-07-08 15:24:40 +0600

    Basic Webdriver Methods

--- pythonSelenium/demoBrowser.py
+++ pythonSelenium/demoBrowser.py
@@ -1,10 +1,34 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
-# service_obj = Service("../resources/chromedriver.exe")
+service_obj = Service("../resources/chromedriver.exe")
+
+# Add some extra option for Chrome
+options = Options()
+options.add_argument("start-maximized")
 
 # Auto Download Driver & Saved into Cache
-service_obj = Service(ChromeDriverManager().install())
-driver = webdriver.Chrome(service=service_obj)
-driver.get("https://github.com/SeleniumHQ/selenium/tree/trunk/py")
+# service_obj = Service(ChromeDriverManager().install())
+
+# Initialize Chrome Driver & Ready to go
+driver = webdriver.Chrome(service=service_obj, options=options)
+
+# Open browser & go to URL
+URL = "https://rahulshettyacademy.com/"
+driver.get(URL)
+# assert webpage title
+assert driver.title in "Rahul Shetty Academy"
+# Checking Current URL
+assert driver.current_url in URL
+driver.get("https://www.rahulshettyacademy.com/AutomationPractice/")
+# Minimize the window
+driver.minimize_window()
+# Back, Refresh & Forward the browser
+driver.back()
+driver.refresh()
+driver.maximize_window()
+driver.forward()
+# Close the driver
+driver.close()
