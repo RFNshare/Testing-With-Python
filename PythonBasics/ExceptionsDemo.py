fb106eb7adf1a62337cad06a8176f16512cd5e00
RFNshare <rfnshare@gmail.com>
2022-07-08 10:33:00 +0600

    try, except & finally concepy

--- PythonBasics/ExceptionsDemo.py
+++ PythonBasics/ExceptionsDemo.py
@@ -1,6 +1,17 @@
 ItemsInCart = 0
 # 2 Product item will be added to cart
 
-assert ItemsInCart == 2
-if ItemsInCart != 2:
-    raise Exception("Products Cart Count Not Matching")
+# assert ItemsInCart == 2
+# if ItemsInCart != 2:
+#     raise Exception("Products Cart Count Not Matching")
+
+# try, except
+path = '../resources/sample.txt'
+try:
+    with open(path, 'r') as reader:
+        reader.read()
+except Exception as e:
+    print(e, "File name is wrong")
+
+finally:
+    print('Try Catch Complete, Cleaning Record....')
