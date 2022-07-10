d1bcd779be2ad693f68e2ca76286d0a331d86723
RFNshare <rfnshare@gmail.com>
2022-07-10 19:35:24 +0600

    Xpath & CSS locator

--- pythonSelenium/demoLocators.py
+++ pythonSelenium/demoLocators.py
@@ -11,8 +11,18 @@ driver = webdriver.Edge(service=service_obj)
 driver.get("https://rahulshettyacademy.com/angularpractice/")
 driver.maximize_window()
 # Inspecting Element By ID, Xpath, CSSSelector, Classname, Name
+driver.find_element(By.CSS_SELECTOR, "input[name='name']").send_keys("Abdullah")
 driver.find_element(By.NAME, "email").send_keys("randomemail@gmail.com")
 driver.find_element(By.ID, "exampleInputPassword1").send_keys("123456")
 driver.find_element(By.ID, "exampleCheck1").click()
+
+# xpath: //tagname[@attribute='value']
+# css: tagname[attribute='value'] / #id, .classname
+driver.find_element(By.CSS_SELECTOR, "#inlineRadio1").click()
+# driver.find_element(By.CSS_SELECTOR, 'input[type="text"]')
+driver.find_element(By.XPATH,'(//input[@type="text"])[3]').send_keys("Nothing Phone 1")
+driver.find_element(By.XPATH, "//input[@type='submit']").click()
+success = driver.find_element(By.CLASS_NAME, "alert-success").text
+assert "Success" in success
 time.sleep(5)
 driver.close()
