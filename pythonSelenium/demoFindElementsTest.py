e0d0a2218c9ff6832cdd85e6076c69ac9b2627af
RFNshare <rfnshare@gmail.com>
2022-07-11 15:15:16 +0600

    Handle Dynamic Dropdowns

---
+++ pythonSelenium/demoFindElementsTest.py
@@ -0,0 +1,32 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
+from selenium.webdriver.edge.service import Service as EdgeService
+
+service_obj = EdgeService(EdgeChromiumDriverManager().install())
+driver = webdriver.Edge(service=service_obj)
+
+driver.get("https://rahulshettyacademy.com/dropdownsPractise/")
+driver.maximize_window()
+driver.find_element(By.CSS_SELECTOR, "input[class~='inputs']").send_keys("Ind")
+time.sleep(2)
+search_results = driver.find_elements(By.CSS_SELECTOR, "li[class^='ui'] a")
+print(f"Found Total {len(search_results)} Countries")
+# lst = []
+# a = 0
+# for i in search_results:
+#     a += 1
+#     print("Inserting " + i.text + " into list", end='')
+#     for j in range(a):
+#         print(".", end='')
+#     lst.append(i.text)
+#     print("\n")
+#     time.sleep(0.5)
+# print(lst)
+for i in search_results:
+    if i.text == "India":
+        i.click()
+assert "India" in driver.find_element(By.CSS_SELECTOR, "input[class~='inputs']").get_attribute("value")
