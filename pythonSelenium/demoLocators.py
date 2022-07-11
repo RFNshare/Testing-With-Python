f376b91ae0ece08aa3599770d868d4a81d5b4715
RFNshare <rfnshare@gmail.com>
2022-07-11 14:27:33 +0600

    Handle static dropdown

--- pythonSelenium/demoLocators.py
+++ pythonSelenium/demoLocators.py
@@ -2,6 +2,7 @@ import time
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
 from webdriver_manager.microsoft import EdgeChromiumDriverManager
 from selenium.webdriver.edge.service import Service as EdgeService
 
@@ -19,6 +20,13 @@ driver.find_element(By.ID, "exampleCheck1").click()
 # xpath: //tagname[@attribute='value']
 # css: tagname[attribute='value'] / #id, .classname
 driver.find_element(By.CSS_SELECTOR, "#inlineRadio1").click()
+
+# Static Dropdown
+gender = Select(driver.find_element(By.ID, "exampleFormControlSelect1"))
+gender.select_by_visible_text("Female")
+gender.select_by_index(0)
+
+
 # driver.find_element(By.CSS_SELECTOR, 'input[type="text"]')
 driver.find_element(By.XPATH, '(//input[@type="text"])[3]').clear()
 driver.find_element(By.XPATH, '(//input[@type="text"])[3]').send_keys("Nothing Phone 1")
@@ -30,4 +38,5 @@ print(a[2].get_attribute("value"))
 for i in a:
     print(i.is_displayed())
 print(driver.find_element(By.CLASS_NAME, "alert-success").value_of_css_property("padding-right"))
+time.sleep(5)
 driver.close()
