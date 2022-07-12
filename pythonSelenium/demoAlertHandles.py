d5a85d7e65230fe7a037dc5d635008f6ab4b449b
RFNshare <rfnshare@gmail.com>
2022-07-12 12:22:32 +0600

    Handle alerts, added display assert

---
+++ pythonSelenium/demoAlertHandles.py
@@ -0,0 +1,25 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
+from selenium.webdriver.edge.service import Service as EdgeService
+
+service_obj = EdgeService(EdgeChromiumDriverManager().install())
+driver = webdriver.Edge(service=service_obj)
+
+driver.get("https://rahulshettyacademy.com/AutomationPractice/")
+driver.maximize_window()
+driver.find_element(By.CSS_SELECTOR, "#name").send_keys("Faroque")
+driver.find_element(By.CSS_SELECTOR, "#alertbtn").click()
+alert = driver.switch_to.alert
+print(alert.text)
+assert "Faroque" in alert.text
+alert.accept()
+time.sleep(1)
+driver.find_element(By.CSS_SELECTOR, "#name").send_keys("Faroque")
+driver.find_element(By.CSS_SELECTOR, "#confirmbtn").click()
+time.sleep(1)
+alert.dismiss()
+time.sleep(3)
--- pythonSelenium/demoAutomationPractice.py
+++ pythonSelenium/demoAutomationPractice.py
@@ -30,3 +30,4 @@ time.sleep(0.1)
 driver.find_element(By.CSS_SELECTOR, "input[class~='btn-style']:nth-child(3)").click()
 assert driver.find_element(By.CSS_SELECTOR,
                            "div[class^='right']:nth-child(2) fieldset:nth-child(1) [placeholder*='ide']").is_displayed()
+
