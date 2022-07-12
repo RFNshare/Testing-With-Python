d920fd9e21948456e83325dd17d8a2c4e1d277da
RFNshare <rfnshare@gmail.com>
2022-07-12 16:13:08 +0600

    Applied explicit wait for greenkart site

--- pythonSelenium/demoWaitElement.py
+++ pythonSelenium/demoWaitElement.py
@@ -2,7 +2,9 @@ import time
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.select import Select
+from selenium.webdriver.support.wait import WebDriverWait
 from webdriver_manager.microsoft import EdgeChromiumDriverManager
 from selenium.webdriver.edge.service import Service as EdgeService
 
@@ -11,7 +13,8 @@ driver = webdriver.Edge(service=service_obj)
 
 driver.get("https://rahulshettyacademy.com/seleniumPractise")
 driver.maximize_window()
-driver.implicitly_wait(5)
+# Implicit Waiting in selenium
+driver.implicitly_wait(1)
 driver.find_element(By.CSS_SELECTOR, "[placeholder*='for']").send_keys("ber")
 time.sleep(3)
 products = driver.find_elements(By.CSS_SELECTOR, "div[class^='product'] [type*='button']")
@@ -22,6 +25,14 @@ driver.find_element(By.CSS_SELECTOR, ".cart-icon").click()
 driver.find_element(By.CSS_SELECTOR, "div[class^='action']:nth-child(2) button[type='button']").click()
 driver.find_element(By.CSS_SELECTOR, ".promoCode").send_keys("rahulshettyacademy")
 driver.find_element(By.CSS_SELECTOR, ".promoBtn").click()
+
+# Explicit wait in selenium
+wait = WebDriverWait(driver, 10)
+# Wait Disable
+wait.until_not(EC.presence_of_element_located((By.CSS_SELECTOR, ".promo-btn-loader")))
+print("Just Now")
+# Wait Enable
+wait.until(EC.text_to_be_present_in_element((By.CSS_SELECTOR, ".promoInfo"), "Code applied ..!"))
 success = driver.find_element(By.CSS_SELECTOR, ".promoInfo").text
 print(success)
 assert "applied" in success
