288ff35b33a7fa3f1ab5ee5c4c3a23174e0e6fa6
Abdullah Al Faroque <rfnshare@gmail.com>
2023-03-10 20:44:16 +0600

    Basic Locator, XPATH & CSS Selector

--- v2/pythonSelenium/locators.py
+++ v2/pythonSelenium/locators.py
@@ -12,8 +12,15 @@ URL = "https://rahulshettyacademy.com/angularpractice"
 driver.get(URL)
 
 # ID, Xpath, CSSSelector, CLassName, Name, LinkText
-driver.find_element(By.NAME, "name").send_keys("Faroque")
+driver.find_element(By.CSS_SELECTOR, "input[name='name']").send_keys("Abdullah Al Faroque")
 driver.find_element(By.NAME, "email").send_keys("aalfaroque@gmail.com")
 driver.find_element(By.ID, "exampleInputPassword1").send_keys("123456")
 driver.find_element(By.ID, "exampleCheck1").click()
-time.sleep(5)
+
+# Xpath Syntext: //tag[@attribute='value']
+# CSS Syntext:   tag[attribute='value']
+driver.find_element(By.XPATH, "//input[@type='submit']").click()
+msg = driver.find_element(By.CLASS_NAME, "alert-success").text
+print(msg)
+assert "Success" in msg
+time.sleep(3)
