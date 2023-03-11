7a0c76f78d6fa2dac4342e1b26c3ea43b9803386
Abdullah Al Faroque <rfnshare@gmail.com>
2023-03-11 16:30:23 +0600

    Handle & Validate Radio Buttons

--- v2/pythonSelenium/uiControl.py
+++ v2/pythonSelenium/uiControl.py
@@ -17,4 +17,12 @@ for checkbox in checkboxes:
         checkbox.click()
         assert checkbox.is_selected()
         break
+
+radios = driver.find_elements(By.XPATH, "//input[@type='radio']")
+
+for radio in radios:
+    if radio.get_attribute("value") == "radio3":
+        radio.click()
+        assert radio.is_selected()
+        break
 time.sleep(2)
