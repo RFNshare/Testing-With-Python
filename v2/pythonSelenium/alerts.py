e1c6aef571cf161ac0a10d2085af5a18f2b0c3f1
Abdullah Al Faroque <rfnshare@gmail.com>
2023-03-11 19:23:19 +0600

    Handaling alert in selenium

---
+++ v2/pythonSelenium/alerts.py
@@ -0,0 +1,21 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
+from webdriver_manager.chrome import ChromeDriverManager
+
+service = Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service)
+driver.maximize_window()
+URL = "https://rahulshettyacademy.com/AutomationPractice"
+driver.get(URL)
+name = "Faroque"
+driver.find_element(By.CSS_SELECTOR, "#name").send_keys(name)
+driver.find_element(By.CSS_SELECTOR, "#alertbtn").click()
+alert = driver.switch_to.alert
+print(alert.text)
+assert name in alert.text
+alert.accept()
+# alert.dismiss()
+time.sleep(2)
