ae77901e649779b283634cc1c86428cf6ab7488f
Abdullah Al Faroque <rfnshare@gmail.com>
2023-03-11 19:58:54 +0600

    Implicit wait in selenium

--- v2/pythonSelenium/waitDemo.py
+++ v2/pythonSelenium/waitDemo.py
@@ -10,6 +10,7 @@ driver = webdriver.Chrome(service=service)
 driver.maximize_window()
 URL = "https://rahulshettyacademy.com/seleniumPractise"
 driver.get(URL)
+driver.implicitly_wait(60)
 driver.find_element(By.CSS_SELECTOR, ".search-keyword").send_keys("Ber")
 time.sleep(3)
 products = driver.find_elements(By.CSS_SELECTOR, ".product-action")
@@ -17,4 +18,11 @@ assert len(products) > 0
 for product in products:
     # Chaining Locators
     product.find_element(By.CSS_SELECTOR, "button").click()
+
+driver.find_element(By.CSS_SELECTOR, ".cart-icon").click()
+driver.find_element(By.XPATH, "//button[text()='PROCEED TO CHECKOUT']").click()
+driver.find_element(By.CSS_SELECTOR, ".promoCode").send_keys("rahulshettyacademy")
+driver.find_element(By.CSS_SELECTOR, ".promoBtn").click()
+msg = driver.find_element(By.CLASS_NAME, "promoInfo").text
+print(msg)
 time.sleep(3)
