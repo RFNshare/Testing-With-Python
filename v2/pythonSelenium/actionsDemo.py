5d773cfb29dcf428e5a53545d6fbde84515a5444
Abdullah Al Faroque <rfnshare@gmail.com>
2023-03-13 08:32:54 +0600

    hover over & perform actions using actionchains

---
+++ v2/pythonSelenium/actionsDemo.py
@@ -0,0 +1,25 @@
+import time
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.support.wait import WebDriverWait
+from webdriver_manager.chrome import ChromeDriverManager
+
+service = Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service)
+driver.maximize_window()
+URL = "https://rahulshettyacademy.com/AutomationPractice"
+driver.get(URL)
+
+action = ActionChains(driver)
+# action.double_click()
+# action.context_click()
+# action.drag_and_drop()
+# action.click_and_hold()
+action.move_to_element(driver.find_element(By.ID, "mousehover")).perform()
+# action.context_click(driver.find_element(By.LINK_TEXT, "Top")).perform()
+action.move_to_element(driver.find_element(By.LINK_TEXT, "Reload")).click().perform()
+time.sleep(3)
