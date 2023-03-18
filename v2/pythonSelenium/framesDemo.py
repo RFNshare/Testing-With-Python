f4c29effc27577ef12b179f5ddc72e9425d65163
Abdullah Al Faroque <rfnshare@gmail.com>
2023-03-18 21:32:59 +0600

    frame handaling

---
+++ v2/pythonSelenium/framesDemo.py
@@ -0,0 +1,19 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
+from webdriver_manager.chrome import ChromeDriverManager
+
+service = Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service)
+driver.maximize_window()
+URL = "https://the-internet.herokuapp.com/iframe"
+driver.get(URL)
+driver.switch_to.frame("mce_0_ifr")
+driver.find_element(By.ID, "tinymce").clear()
+driver.find_element(By.ID, "tinymce").send_keys("Writing Some Texts")
+driver.switch_to.default_content()
+print(driver.find_element(By.TAG_NAME, "h3").text)
+time.sleep(2)
+driver.quit()
---
+++ v2/pythonSelenium/framesPractice.py
@@ -0,0 +1,16 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
+service = Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service)
+driver.maximize_window()
+URL = "https://rahulshettyacademy.com/AutomationPractice/"
+driver.get(URL)
+driver.switch_to.frame("courses-iframe")
+lst = driver.find_elements(By.XPATH, "//div[@id='showHeader']//b")
+print(lst[0].text)
+driver.switch_to.default_content()
+print(driver.find_element(By.TAG_NAME, "h1").text)
