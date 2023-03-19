bffddcd4f94ca1f5628f2fb56606646cac83fce0
Abdullah Al Faroque <rfnshare@gmail.com>
2023-03-19 22:38:07 +0600

    using xfail mark to skip fail report in a test set

--- pytestsDemo/test_demoTwo.py
+++ pytestsDemo/test_demoTwo.py
@@ -1,12 +1,15 @@
 import pytest
 
 
-@pytest.mark.smoke
+@pytest.mark.xfail
 def test_third_program():
     msg = "Hello"
+    print("Skip but not skipped")
     assert msg == "Hi", "Test Failed Because strings do not match"
 
 
+@pytest.mark.smoke
+@pytest.mark.skip
 def test_fourth_program_debit_card():
     a = 2
     b = 6
