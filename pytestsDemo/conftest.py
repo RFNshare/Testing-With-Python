e3c132e33b2e414928b72598d31e40bae7e54d5c
Abdullah Al Faroque <rfnshare@gmail.com>
2023-03-19 23:30:27 +0600

    added some imp fixture

--- pytestsDemo/conftest.py
+++ pytestsDemo/conftest.py
@@ -1,8 +1,35 @@
 import pytest
 
 
+# Scope on class level
 @pytest.fixture(scope="class")
-def setup():
-    print("Setup Processing...")
+def setup_data():
+    print("Initialize Variables For Set...")
     yield
-    print("Teardown ...")
+    print("Cleanup Test Cases Data ...")
+
+
+# Scope on module level
+@pytest.fixture()
+def setup_browser():
+    print("Open Browser...")
+    yield
+    print("Teardown Browser ...")
+
+# @pytest.fixture
+# def driver():
+#     _driver = Chrome()
+#     yield _driver
+#     _driver.quit()
+#
+#
+# @pytest.fixture
+# def login(driver, base_url, user):
+#     driver.get(urljoin(base_url, "/login"))
+#     page = LoginPage(driver)
+#     page.login(user)
+#
+#
+# @pytest.fixture
+# def landing_page(driver, login):
+#     return LandingPage(driver)
