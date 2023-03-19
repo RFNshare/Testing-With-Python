21e935586ff660bbc10cd48a7334ade0301d7fae
Abdullah Al Faroque <rfnshare@gmail.com>
2023-03-20 00:15:22 +0600

    update test class with loaded fixture data, use extra fixture in a specfic method to load data

--- pytestsDemo/test_FixtureDemo.py
+++ pytestsDemo/test_FixtureDemo.py
@@ -1,10 +1,19 @@
 import pytest
+from selenium.webdriver.common.by import By
 
 
+# setup data class level
+# setup browser method level
+# data load specific method level
 @pytest.mark.usefixtures("setup_data", "setup_browser")
 class TestExample:
-    def test_fix_demo_one(self):
-        print("Test in fixtureDemo")
+    def test_fix_demo_one(self, driver, data_load):
+        print("Test in heroku app demo")
+        driver.get(data_load[0])
+        driver.switch_to.frame("mce_0_ifr")
+        driver.find_element(By.ID, "tinymce").clear()
+        print("Load Data From DataLoad Fixture:", data_load)
+        driver.find_element(By.ID, "tinymce").send_keys(data_load[1])
 
     def test_fix_demo_two(self):
         print("Test in fixtureDemo")
