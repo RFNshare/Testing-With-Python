791b927d10de53f8a91d3149d777f401f307b794
Abdullah Al Faroque <rfnshare@gmail.com>
2023-03-21 15:16:40 +0600

    update testcases with log option

--- pytestsDemo/test_FixtureDemo.py
+++ pytestsDemo/test_FixtureDemo.py
@@ -1,23 +1,28 @@
 import pytest
 from selenium.webdriver.common.by import By
+from pytestsDemo.BaseClass import BaseClass
 
 
 # setup data class level
 # setup browser method level
 # data load specific method level
 @pytest.mark.usefixtures("setup_data", "setup_browser")
-class TestExample:
+class TestExample(BaseClass):
     @pytest.mark.smoke
     def test_fix_demo_one(self, driver, setup_data):
-        print("Test in heroku app demo")
+        log = self.get_logger()
+        log.debug("Test in heroku app demo")
         driver.get(setup_data[0])
         driver.switch_to.frame("mce_0_ifr")
         driver.find_element(By.ID, "tinymce").clear()
         # print("Load Data From DataLoad Fixture:", setup_data)
         driver.find_element(By.ID, "tinymce").send_keys(setup_data[1])
+        log.info(f"Data Send Successfully in {setup_data[1]}")
 
-    def test_fix_demo_two(self):
-        print("Test in fixtureDemo Two")
+    def test_fix_demo_two(self, cross_browser):
+        log = self.get_logger()
+        log.debug("Test in fixtureDemo Two")
+        log.info(f"Execute Successfully in {cross_browser} Browser")
 
     def test_fix_demo_three(self):
         print("Test in fixtureDemo Three")
