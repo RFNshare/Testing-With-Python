572b5b24a1ba61d981998648ba54297046b68730
Abdullah Al Faroque <rfnshare@gmail.com>
2023-03-24 16:16:31 +0600

    added command line argument to select dynamic browser

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,24 +2,49 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
 
 
+def pytest_addoption(parser):
+    parser.addoption(
+        "--browser_name", action="store", default="chrome", help="browser options: chrome/firefox/edge/all",
+        choices=("chrome", "firefox", "edge", "all")
+    )
+
+
+@pytest.fixture(params=["chrome", "firefox", "edge"])
+def cross_browser(request):
+    return request.param
+
+# Pending Cross Browser Implementation
 @pytest.fixture(scope="class")
 def setup(request):
-    URL = "https://rahulshettyacademy.com/angularpractice/shop"
-    # Adding Additional Options
-    options = webdriver.ChromeOptions()
-    # options.add_argument("--start-maximized")
-    # options.add_argument("headless")
-    # options.add_argument('window-size=1920,1080')
+    _driver = None
+    browser_name = request.config.getoption("browser_name")
+    if browser_name == "chrome":
+        # Adding Additional Options
+        options = webdriver.ChromeOptions()
+        # options.add_argument("--start-maximized")
+        # options.add_argument("headless")
+        # options.add_argument('window-size=1920,1080')
+        service = Service(ChromeDriverManager().install())
+        service = Service(ChromeDriverManager().install())
+        _driver = webdriver.Chrome(service=service, options=options)
+    elif browser_name == "firefox":
+        service = Service(GeckoDriverManager().install())
+        _driver = webdriver.Firefox(service=service)
+    elif browser_name == "edge":
+        service = Service(EdgeChromiumDriverManager().install())
+        _driver = webdriver.Edge(service=service)
 
+    URL = "https://rahulshettyacademy.com/angularpractice/shop"
     # Initialize Browser
-    service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service, options=options)
-    driver.get(URL)
-    driver.implicitly_wait(5)
-    driver.maximize_window()
-    request.cls.driver = driver  # Initialize driver as class variable. whoever class use this fixture they can use
+
+    _driver.get(URL)
+    _driver.implicitly_wait(5)
+    _driver.maximize_window()
+    request.cls.driver = _driver  # Initialize driver as class variable. whoever class use this fixture they can use
     # this variable
-    yield driver
-    driver.quit()
+    yield _driver
+    _driver.quit()
