bfb17865ccf807df637581296113a9265ba9dee8
Abdullah Al Faroque <rfnshare@gmail.com>
2023-03-25 11:06:51 +0600

    data driven testing using dic in params

---
+++ testData/HomePageData.py
@@ -0,0 +1,3 @@
+class HomePageData:
+    test_homepage_data = [{"first_name": "Abdullah Al", "email": "aalfaroque@gmail.com", "password": "12345"},
+                            {"first_name": "Faroque", "email": "rfnshare@gmail.com", "password": "4321"}]
--- tests/test_home.py
+++ tests/test_home.py
@@ -1,6 +1,7 @@
 import pytest
 
 from pageObjects.HomePage import HomePage
+from testData.HomePageData import HomePageData
 from utilities.BaseClass import BaseClass
 from utilities.data import SampleData
 
@@ -8,19 +9,18 @@ from utilities.data import SampleData
 class TestHomePage(BaseClass):
     def test_form_submit(self, get_data):
         home_page = HomePage(self.driver)
-        home_page.get_name().send_keys(get_data[0])
-        home_page.get_email().send_keys(get_data[1])
-        home_page.get_pass().send_keys(get_data[2])
+        home_page.get_name().send_keys(get_data["first_name"])
+        home_page.get_email().send_keys(get_data["email"])
+        home_page.get_pass().send_keys(get_data["password"])
         home_page.get_checkbox().click()
         self.select_by_visible_text(home_page.get_gender_dropdown(), "Male")
         home_page.get_radio().click()
         home_page.get_submit().click()
         msg = home_page.get_success_msg()
         assert SampleData.success_message in msg
-        assert home_page.get_text_binding() == get_data[0]
+        assert home_page.get_text_binding() == get_data["first_name"]
         self.driver.refresh()
 
-    @pytest.fixture(
-        params=[("Abdullah Al", "aalfaroque@gmail.com", "12345"), ("Faroque", "rfnshare@gmail.com", "4321")])
+    @pytest.fixture(params=HomePageData.test_homepage_data)
     def get_data(self, request):
         return request.param
