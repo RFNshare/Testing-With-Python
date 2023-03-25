73ca120996f54d783e2c5d870960788ff51a01d7
Abdullah Al Faroque <rfnshare@gmail.com>
2023-03-26 00:22:11 +0600

    wrap up excel learning

--- OthersLearnPractice/pytestsDemo/excel_demo_two.py
+++ OthersLearnPractice/pytestsDemo/excel_demo_two.py
@@ -10,11 +10,13 @@ for i in range(1, 2):
     for j in range(1, sheet.max_column + 1):
         keys.append(sheet.cell(row=i, column=j).value)
 for i in range(2, sheet.max_row + 1):
-    for j in range(1, sheet.max_column + 1):
-        values[k].append(sheet.cell(row=i, column=j).value)
+    if sheet.cell(row=i, column=1).value == "TestCase2":
+        for j in range(1, sheet.max_column + 1):
+            values[k].append(sheet.cell(row=i, column=j).value)
     k += 1
 lst_dic = []
 for i in values:
     dic = dict(zip(keys, i))
-    lst_dic.append(dic)
+    if dic:
+        lst_dic.append(dic)
 print(lst_dic)
