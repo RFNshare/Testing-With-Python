d8a08b50152d8339fa3e5595c3cfad0e0d934dcd
Abdullah Al Faroque <rfnshare@gmail.com>
2023-03-25 13:41:26 +0600

    added method for taking ss when test case fails

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
@@ -6,6 +8,7 @@ from webdriver_manager.firefox import GeckoDriverManager
 from webdriver_manager.microsoft import EdgeChromiumDriverManager
 
 
+_driver = None
 def pytest_addoption(parser):
     parser.addoption(
         "--browser_name", action="store", default="chrome", help="browser options: chrome/firefox/edge/all",
@@ -21,7 +24,7 @@ def cross_browser(request):
 # Pending Cross Browser Implementation
 @pytest.fixture(scope="class")
 def setup(request):
-    _driver = None
+    global _driver
     browser_name = request.config.getoption("browser_name")
     if browser_name == "chrome":
         # Adding Additional Options
@@ -49,3 +52,31 @@ def setup(request):
     # this variable
     yield _driver
     _driver.quit()
+
+
+@pytest.mark.hookwrapper
+def pytest_runtest_makereport(item):
+    """
+        Extends the PyTest Plugin to take and embed screenshot in html report, whenever test fails.
+        :param item:
+        """
+    pytest_html = item.config.pluginmanager.getplugin('html')
+    outcome = yield
+    report = outcome.get_result()
+    extra = getattr(report, 'extra', [])
+
+    if report.when == 'call' or report.when == "setup":
+        xfail = hasattr(report, 'wasxfail')
+        if (report.skipped and xfail) or (report.failed and not xfail):
+            file_name = (report.nodeid.replace("::","_")).replace("/","__")+".png"
+            SS_PATH = Path(__file__).parent / "../output/screenshots"
+            _capture_screenshot(SS_PATH / file_name)
+            if file_name:
+                html = '<div><img src="../screenshots/%s" alt="screenshot" style="width:304px;height:228px;" ' \
+                       'onclick="window.open(this.src)" align="right"/></div>' % file_name
+                extra.append(pytest_html.extras.html(html))
+        report.extra = extra
+
+
+def _capture_screenshot(name):
+    _driver.get_screenshot_as_file(name)
