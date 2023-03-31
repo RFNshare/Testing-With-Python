148707926839ed97c5410b114a03ed5d65049090
Abdullah Al Faroque <rfnshare@gmail.com>
2023-03-31 22:12:07 +0600

    pytest fixture, parameterize & pytest raise assert

--- LearnPyest/gist/test_gist.py
+++ LearnPyest/gist/test_gist.py
@@ -28,3 +28,40 @@ def test_with_custom_mark1() -> None:
 @pytest.mark.slow
 def test_with_custom_mark2() -> None:
     pass
+
+
+class Company:
+    def __init__(self, name: str, stock_symbol: str):
+        self.name = name
+        self.stock_symbol = stock_symbol
+
+    def __str__(self):
+        return f"{self.name}:{self.stock_symbol}"
+
+
+@pytest.fixture
+def company() -> Company:
+    return Company(name="Fiver", stock_symbol="FVRR")
+
+
+def test_with_fixture(company: Company) -> None:
+    print(f"Printing {company} from fixture")
+
+
+@pytest.mark.parametrize(
+    "company_name",
+    ["TikTok", "Instagram", "Twitch"],
+    ids=["TICKTOK TEST", "INSTA TEST", "TWITCH TEST"]
+)
+def test_params(company_name: str) -> None:
+    print(f"\nTest With {company_name}")
+
+
+def raise_covid19_exception() -> None:
+    raise ValueError("CoronaVirus Exception")
+
+
+def test_raise_covid19_exception_should_pass() -> None:
+    with pytest.raises(ValueError) as e:
+        raise_covid19_exception()
+    assert "CoronaVirus Exception" == str(e.value)
